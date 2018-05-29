6451ee44d1a84f6143bdf7e5fc8c93fdeb600310
PopovDmytro <dimahom@ukr.net>
2018-05-29 23:13:49 +0300

    functional, prototype js OOP

--- app/scripts/fundamentals_itvdn.js
+++ app/scripts/fundamentals_itvdn.js
@@ -1,4 +1,7 @@
 "use strict";
+//ECMA 5 OOP
+//****************************************************************
+
 // -------
 //functional OOP
 function Rectangle(w, h) {
@@ -69,6 +72,83 @@ function showCtor(obj, name) {
 
 //-------
 RectangleProto.prototype.equals = function (otherObj) {
-  return (this.width === otherObj.width && this.height === otherObj.height);
+    return (this.width === otherObj.width && this.height === otherObj.height);
 };
 // console.log(rec1.equals(rec2));
+RectangleProto.prototype.compareTo = function (otherObj) {
+    if (this.getArea() > otherObj.getArea()) {
+        return 1;
+    } else if (this.getArea() < otherObj.getArea()) {
+        return -1;
+    } else {
+        return 0;
+    }
+    // return this.getArea() - otherObj.getArea();
+};
+
+//-------
+//encapsulation
+const MyPreviewClass = function () {
+
+    //private method
+    const privateMEthod = function () {
+        console.log('private method');
+    };
+
+    //public method
+    this.publicMethod = function () {
+        console.log('public method');
+        //here ypu can use private
+        privateMEthod();
+    };
+
+    return this;
+};
+
+let m = new MyPreviewClass();
+// m.publicMethod();
+
+//-------
+//inheritance
+function Human (name) {
+    this.name = name;
+    this.talk = function () {
+        console.log(`Hello my name is ${this.name} \n`);
+    }
+}
+
+function Student (name) {
+    this.name = name;
+    this.school = "CBS School";
+}
+
+function Worker (name) {
+    this.name = name;
+    this.speciality = "Software developer";
+}
+
+//create parent object ---
+const human = new Human('name');
+//inherit human as prototype ---
+Student.prototype = human;
+Worker.prototype = human;
+//
+
+const Alex = new Student('Alex');
+const Andrew = new Worker('Andrew');
+Alex.talk();
+Andrew.talk();
+
+//-------
+//polymorphism
+function Teacher(name) {
+    this.name = name;
+    this.speciality = "Teacher";
+
+    this.talk = function () {
+        console.log(`Hello I am a teacher, my name is ${this.name} \n`);
+    }
+}
+Teacher.prototype = human;
+const Isaak = new Teacher('Isaak');
+Isaak.talk();
