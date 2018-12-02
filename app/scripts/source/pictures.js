52f8e9c98b56e3f193cef0011c100f2f6144a4a3
PopovDmytro <dimahom@ukr.net>
2018-12-02 21:42:41 +0200

    lesson 3 , shows big picture and set comments li to list

--- app/scripts/source/pictures.js
+++ app/scripts/source/pictures.js
@@ -1,3 +1,8 @@
+//load avatar images
+const imagesArr = [];
+for (let i = 1; i < 7; i++) {
+    imagesArr.push(require(`../../images/avatar-${i}.svg`));
+}
 //
 const templateArrOfComments = ['Всё отлично!', 'В целом всё неплохо. Но не всё.', 'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.', 'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.', 'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.', 'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!',];
 const templateArrOfDescriptions = ['Тестим новую камеру!', 'Затусили с друзьями на море', 'Как же круто тут кормят', 'Отдыхаем...', 'Цените каждое мгновенье. Цените тех, кто рядом с вами и отгоняйте все сомненья. Не обижайте всех словами......', 'Вот это тачка!',];
@@ -6,7 +11,27 @@ const templateData = randomDataGenerator(templateArrOfComments, templateArrOfDes
 
 renderPosts(templateData, '.pictures', '#picture');
 
-//functions
+//big-picture showing and set data from templateData
+const bigPicture = document.querySelector('.big-picture');
+bigPicture.classList.remove('hidden');
+bigPicture.querySelector('.big-picture__img img').setAttribute('src', templateData[0].url);
+bigPicture.querySelector('.likes-count').textContent = templateData[0].likes;
+bigPicture.querySelector('.social__comment-count .comments-shows').textContent = templateData[0].comments.length;
+bigPicture.querySelector('.social__caption').textContent = templateData[0].description;
+bigPicture.querySelector('.comments-count').classList.add('visually-hidden');
+bigPicture.querySelector('.social__comments-loader').classList.add('visually-hidden');
+
+const commentsList = document.querySelector('.social__comments');
+const commentsFragment = document.createDocumentFragment();
+for(let i = 0; i < templateData[0].comments.length; i++) {
+    const commentLi = createCommentDOMElement(templateData[0].comments[i]);
+    commentsFragment.appendChild(commentLi);
+}
+commentsList.appendChild(commentsFragment);
+
+/*
+* functions
+*/
 
 //insert elements into block
 function renderPosts(dataArr, blockSelector, templateSelector) {
@@ -22,7 +47,7 @@ function renderPosts(dataArr, blockSelector, templateSelector) {
 
     document.querySelector(blockSelector).appendChild(fragment);
 }
-//create DOM element
+//create post DOM element
 function createPostDOMElement(dataObj, template) {
     const newTemplate = template.content.cloneNode(true);
 
@@ -40,6 +65,27 @@ function createPostDOMElement(dataObj, template) {
     );
 
     return newTemplate;
+}
+//create comment DOM element
+function createCommentDOMElement(text, dataObj, template) {
+    const commentLi = document.createElement('li');
+    commentLi.className = 'social__comment social__comment--text';
+
+    const commentAvatarImg = document.createElement('img');
+    commentAvatarImg.setAttribute('src', imagesArr[randomN(0,5)]);
+    commentAvatarImg.className = 'social__picture';
+    commentAvatarImg.setAttribute('alt', 'Photo commentator avatar');
+    commentAvatarImg.setAttribute('width', '35');
+    commentAvatarImg.setAttribute('height', '35');
+
+    const commentText = document.createElement('p');
+    commentText.className = 'social__text';
+    commentText.textContent = text;
+
+    commentLi.appendChild(commentAvatarImg);
+    commentLi.appendChild(commentText);
+
+    return commentLi;
 }
 //create node element and add class
 function createNode(tagName, classes = '', id = '') {
