09dcb0d565ac26a2e1735c70d0bd3ebc0837f760
PopovDmytro <dimahom@ukr.net>
2019-03-24 19:52:22 +0200

    init project

--- src/js/bundle.js
+++ src/js/bundle.js
@@ -1,13 +1,3 @@
-let longVariableNameWithLorem = () => {
-    console.log(123);
-    return 123;
-};
-const h1Tag = document.querySelector('h1');
-h1Tag.innerHTML = "alert";
 
-h1Tag.classList.remove('test');
-setTimeout(() => {
-    h1Tag.classList.add('test');
-}, 3500);
 
 
